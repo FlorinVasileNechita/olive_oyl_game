8a3baf022a03648be5b50e87a49b584ea2fff2f4
Jignesh Darji <jigneshdarji@bitbucket.org>
2015-05-23 12:53:58 +0530

    Change sprites for player and obstacle

---
+++ Assets/Scripts/DownToEarth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DownToEarth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		float 	distanceFromCamera;
+		distanceFromCamera = (transform.position - Camera.main.transform.position).z;
+		float earthYPoint = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceFromCamera)).y;
+
+		Renderer renderer = GetComponent<Renderer> (); //FIXME: Ground does not have a renderer component
+		float spriteCompensation = 0.0f;
+		if (renderer != null)
+			spriteCompensation = renderer.bounds.size.y / 2;
+		transform.position = new Vector3 (transform.position.x,
+		                                 earthYPoint + spriteCompensation,
+		                                 transform.position.z);
+	}
+}
