ccc5945a98cc6f3333c6fb3ab99e474ff404debc
Jignesh Darji <jigneshdarji@bitbucket.org>
2015-05-27 08:59:20 +0530

    Making clouds move at random speed

---
+++ Assets/Scripts/RandomVelocity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomVelocity : MonoBehaviour {
+
+	public Vector2 mVelocityMin, mVelocityMax;
+	void Awake()
+	{
+		Vector2 randomVelocity = new Vector2 (Random.Range (mVelocityMin.x, mVelocityMax.x),
+		                                     Random.Range (mVelocityMin.y, mVelocityMax.y));
+
+		GetComponent<Rigidbody2D>().velocity = randomVelocity;
+	}
+}
--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -3,22 +3,28 @@ using System.Collections;
 
 public class Scroll : MonoBehaviour {
 	
-	public Vector2 mVelocity 		= new Vector2(-5, 0); //Decides the obstacles scrolling speed
+	public Vector2 mVelocity; //Decides the obstacles scrolling speed
 	public float mYAxisRange 				= 4;					
 	float 	mScreenCrossPixelLeft;
 	float 	mScreenCrossPixelRight;
+	float 	mSpriteCompensation;
 	
 	// Use this for initialization
 	void Start()
 	{
-		GetComponent<Rigidbody2D>().velocity = mVelocity;
+		if(mVelocity.x != 0) 
+			GetComponent<Rigidbody2D>().velocity = mVelocity;
 
 		float 	distanceFromCamera;
 		distanceFromCamera = (transform.position - Camera.main.transform.position).z;
-		mScreenCrossPixelLeft = 1.3f * Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceFromCamera)).x;
-		mScreenCrossPixelRight = 1.3f * Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distanceFromCamera)).x;
+		mScreenCrossPixelLeft = /*1.3f * */Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceFromCamera)).x;
+		mScreenCrossPixelRight = /*1.3f * */Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distanceFromCamera)).x;
 
-		transform.position = new Vector3(mScreenCrossPixelRight, 
+		Renderer renderer = GetComponent<Renderer> ();
+		if (renderer != null)
+			mSpriteCompensation = renderer.bounds.size.x / 2;
+
+		transform.position = new Vector3(mScreenCrossPixelRight + mSpriteCompensation, 
 		                                 transform.position.y - mYAxisRange * Random.value, 
 		                                 transform.position.z);
 		//Debug.Log("scrollObstacle::Update() Right: " + screenCrossPixelRight + " Left: " + screenCrossPixelLeft);
@@ -32,15 +38,10 @@ public class Scroll : MonoBehaviour {
 	
 	void CheckLocation()
 	{
-		if (transform.position.x < mScreenCrossPixelLeft) 
+		if (transform.position.x < mScreenCrossPixelLeft - mSpriteCompensation) 
 		{
 			//Destroying this prefab obstacle
 			Destroy (gameObject);
 		} 
-	}
-	
-	void ChangeSpeed(Vector2 velocity)
-	{
-		GetComponent<Rigidbody2D>().velocity = mVelocity;
 	}
 }
