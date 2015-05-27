9580b7ab50e5603c3ff9eeacec0b13a4c855aa2c
Jignesh Darji <jigneshdarji@bitbucket.org>
2015-05-27 09:12:38 +0530

    Resolve issues between heart which ended up killing the heart.

---
+++ Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreCollision : MonoBehaviour {
+
+	public GameObject mObjectToNotCollide;
+
+	void Awake()
+	{
+		Collider2D selfCollider = GetComponent<Collider2D> ();
+		Collider2D otherCollider = mObjectToNotCollide.GetComponent<Collider2D> ();
+
+		Physics2D.IgnoreCollision (selfCollider,
+		                          otherCollider,
+		                          true);
+	}
+}
